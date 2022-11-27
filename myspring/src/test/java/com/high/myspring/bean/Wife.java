35a5747b2c0ed85ed1703f4ec42ef76daf926f6d
high <2303006873@qq.com>
2022-11-27 16:26:18 +0800

    手写Spring之五 测试实例化对象和对象调用方法以及循环依赖问题

---
+++ myspring/src/test/java/com/high/myspring/bean/Wife.java
@@ -0,0 +1,46 @@
+package com.high.myspring.bean;
+
+/**
+ * 妻子类
+ *
+ * @author high
+ * @version 1.0
+ * @since 1.0
+ */
+public class Wife {
+    private String name;
+    private Husband husband;
+
+    public Wife() {
+    }
+
+    public Wife(String name, Husband husband) {
+        this.name = name;
+        this.husband = husband;
+    }
+
+    @Override
+    public String toString() {
+        return "Wife{" +
+                "name='" + name + '\'' +
+                // 这里没有.getName()会循环调用导致StackOverflow栈溢出异常
+                ", husband=" + husband.getName() +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Husband getHusband() {
+        return husband;
+    }
+
+    public void setHusband(Husband husband) {
+        this.husband = husband;
+    }
+}
--- myspring/src/test/java/com/high/myspring/test/MySpringTest.java
+++ myspring/src/test/java/com/high/myspring/test/MySpringTest.java
@@ -1,5 +1,8 @@
 package com.high.myspring.test;
 
+import com.high.myspring.bean.Husband;
+import com.high.myspring.bean.Wife;
+import com.high.myspring.service.UserService;
 import com.high.myspringframework.context.ApplicationContext;
 import com.high.myspringframework.context.support.ClassPathXmlApplicationContext;
 import org.junit.Test;
@@ -15,5 +18,13 @@ public class MySpringTest {
     @Test
     public void testMySpring() {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("myspring.xml");
+        Object user = applicationContext.getBean("user");
+        System.out.println(user);
+        UserService userService = (UserService) applicationContext.getBean("userService");
+        userService.save();
+        Object husband = applicationContext.getBean("husband");
+        Object wife = applicationContext.getBean("wife");
+        System.out.println(husband);
+        System.out.println(wife);
     }
 }
